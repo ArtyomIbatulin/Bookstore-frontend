ae6f1c653a887af3697305ace33d48737345dddf
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-06 17:46:08 +0300

    fixed added baseQuery, api

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,3 +1,29 @@
-import { fetchBaseQuery } from "@reduxjs/toolkit/query"
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query"
+import { BASE_URL } from "../../constants"
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+import { RootState } from "../store"
 
-const baseQuery = fetchBaseQuery({})
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${BASE_URL}/api`,
+  prepareHeaders: (headers, { getState }) => {
+    const token =
+      (getState() as RootState).auth.token || localStorage.getItem("token")
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`)
+    }
+
+    return headers
+  },
+})
+
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 })
+
+export const api = createApi({
+  reducerPath: "splitApi",
+  baseQuery: baseQueryWithRetry,
+  refetchOnMountOrArgChange: true,
+  endpoints: () => ({}),
+})
+
+// eslint-disable!!!
