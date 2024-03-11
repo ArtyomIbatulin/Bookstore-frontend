a4575cd6f3185c7cba961a3c86f6f51569952a5b
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-11 22:24:28 +0300

    added userApi

---
+++ src/app/services/userApi.ts
@@ -0,0 +1,60 @@
+import type { User } from "../types/userType"
+import { api } from "./api"
+
+export const userApi = api.injectEndpoints({
+  endpoints: builder => ({
+    login: builder.mutation<
+      { token: string },
+      { login: string; password: string }
+    >({
+      query: userData => ({
+        url: "/sign-in",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    register: builder.mutation<
+      { login: string; password: string },
+      { login: string; password: string }
+    >({
+      query: userData => ({
+        url: "/sign-up",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    current: builder.query<User, void>({
+      query: () => ({
+        url: "/current-user",
+        method: "GET",
+      }),
+    }),
+    getUserById: builder.query<User, string>({
+      query: id => ({
+        url: `/user/${id}`,
+        method: "GET",
+      }),
+    }),
+    editUser: builder.mutation<User, { userData: FormData; id: string }>({
+      query: ({ userData, id }) => ({
+        url: `/user/${id}`,
+        method: "PUT",
+        body: userData,
+      }),
+    }),
+  }),
+})
+
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useCurrentQuery,
+  useLazyCurrentQuery,
+  useGetUserByIdQuery,
+  useLazyGetUserByIdQuery,
+  useEditUserMutation,
+} = userApi
+
+export const {
+  endpoints: { login, register, current, getUserById, editUser },
+} = userApi
