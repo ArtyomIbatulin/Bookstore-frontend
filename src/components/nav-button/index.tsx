aad8f66a83163721a82f58d6a756f90842783647
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-15 15:39:04 +0300

    added NavButton comp

---
+++ src/components/nav-button/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+import { Button } from "../button"
+
+type Props = {
+  children: React.ReactNode
+  icon: JSX.Element
+  href: string
+}
+
+export const NavButton: React.FC<Props> = ({ children, icon, href }) => {
+  return (
+    <Button className="flex justify-start text-xl" icon={icon}>
+      <Link to={href}>{children}</Link>
+    </Button>
+  )
+}
