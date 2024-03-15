5f5f9c1f60aefffe62f9e02de5c7bfdd4c8d293a
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-15 15:05:37 +0300

    added Container

---
+++ src/components/container/index.tsx
@@ -0,0 +1,7 @@
+type Props = {
+  children: React.ReactElement[] | React.ReactElement
+}
+
+export const Container: React.FC<Props> = ({ children }) => {
+  return <div className="flex max-w-screen-x1 mx-auto mt-10">{children}</div>
+}
