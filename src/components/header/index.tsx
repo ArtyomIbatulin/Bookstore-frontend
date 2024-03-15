d89ce1d470998cf984c24a8d5bccfa9df1c76b77
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-15 14:54:41 +0300

    added header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,28 @@
+import { useContext } from "react"
+import { ThemeContext } from "../theme-provider"
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react"
+import { FaRegMoon } from "react-icons/fa"
+import { LuSunMedium } from "react-icons/lu"
+
+export const Header = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Bookstore</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="lg:flex text-3x1 cursor-pointer">
+          {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
+        </NavbarItem>
+        <NavbarItem></NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
