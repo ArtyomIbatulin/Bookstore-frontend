a26e92ded59c31f75afdf7bed3802f4d41e49974
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-15 19:40:34 +0300

    added AuthPage

--- src/pages/auth-page/index.tsx
+++ src/pages/auth-page/index.tsx
@@ -1,3 +1,30 @@
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react"
+import { useState } from "react"
+
 export const AuthPage = () => {
-  return <div>AuthPage</div>
+  const [selected, setSelected] = useState("login")
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Card className="max-w-full w-[340px] h-[450px]">
+          <CardBody className="overflow-hidden">
+            <Tabs
+              fullWidth
+              size="md"
+              selectedKey={selected}
+              onSelectionChange={key => setSelected(key as string)}
+            >
+              <Tab key="login" title="Вход">
+                Вход
+              </Tab>
+              <Tab key="sign-up" title="Регистрация">
+                Регистрация
+              </Tab>
+            </Tabs>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  )
 }
