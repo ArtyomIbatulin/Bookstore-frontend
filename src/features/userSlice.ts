f235f968fb74505e858679912c4ee5a815ea4f14
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-03-24 21:14:06 +0300

    added userSlice

---
+++ src/features/userSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit"
+import type { User } from "../app/types/userType"
+import { userApi } from "../app/services/userApi"
+
+interface InitialState {
+  user: User | null
+  isAuthenticated: boolean
+  users: User[] | null
+  current: User | null
+  token?: string
+}
+
+const initialState: InitialState = {
+  user: null,
+  isAuthenticated: false,
+  users: null,
+  current: null,
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    logout: () => initialState,
+    resetUser: state => {
+      state.user = null
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addMatcher(userApi.endpoints.login.matchFulfilled, (state, action) => {
+        state.token = action.payload.token
+        state.isAuthenticated = true
+      })
+      .addMatcher(userApi.endpoints.current.matchFulfilled, (state, action) => {
+        state.current = action.payload
+        state.isAuthenticated = true
+      })
+      .addMatcher(
+        userApi.endpoints.getUserById.matchFulfilled,
+        (state, action) => {
+          state.user = action.payload
+        },
+      )
+  },
+})
+
+export const { logout, resetUser } = userSlice.actions
+export default userSlice.reducer
