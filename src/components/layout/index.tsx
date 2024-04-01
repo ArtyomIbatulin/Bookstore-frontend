6faefa42a47e59caba7e82877907750b67ec8105
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-01 20:10:30 +0300

    added check isAuthenticated

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,25 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import { Container } from "../container"
 import { Header } from "../header"
 import { Navbar } from "../navbar"
+import {
+  selectIsAuthenticated,
+  selectUser,
+} from "../../features/user/userSlice"
+import { useAppSelector } from "../../app/hooks"
+import { useEffect } from "react"
 
 export const Layout = () => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated) //useAppSelector
+  const user = useAppSelector(selectUser)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/auth")
+    }
+  }, [isAuthenticated, navigate]) // deps
+
   return (
     <>
       <Header />
