7e57c33b1536a5e66642c6692a76b4637abbcb80
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-07 23:13:24 +0300

    added user comp based NextUI

---
+++ src/components/user/index.tsx
@@ -0,0 +1,27 @@
+import { User as NextUIUser } from "@nextui-org/react"
+import { BASE_URL } from "../../constants"
+
+type Props = {
+  name: string
+  avatarUrl: string
+  description?: string
+  className?: string
+}
+
+export const User: React.FC<Props> = ({
+  name = "",
+  avatarUrl = "",
+  description = "",
+  className = "",
+}) => {
+  return (
+    <NextUIUser
+      name={name}
+      avatarProps={{
+        src: `${BASE_URL}${avatarUrl}`,
+      }}
+      description={description}
+      className={className}
+    />
+  )
+}
