4d93554d8d891a719083b0de7de3e2fab109f024
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-08 21:26:33 +0300

    need fix data object in the MainPage

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -4,25 +4,29 @@ import { CreateBook } from "../../components/create-book"
 
 export const MainPage = () => {
   const { data } = useFindBooksQuery()
+  console.log(typeof data)
+  // data object!!!
 
   return (
     <>
       <div className="mb-10 w-full">
         <CreateBook />
       </div>
-      {data && data.length > 0
-        ? data.map(({ id, name, img, price, description }) => (
-            <Card
-              key={id}
-              id={id}
-              cardFor="book"
-              name={name ?? ""}
-              img={img ?? ""}
-              price={price}
-              description={description ?? ""}
-            />
-          ))
-        : null}
+      {data && data.length > 0 ? (
+        data.map(({ id, name, img, price, description }) => (
+          <Card
+            key={id}
+            id={id}
+            cardFor="book"
+            name={name ?? ""}
+            img={img ?? ""}
+            price={price}
+            description={description ?? ""}
+          />
+        ))
+      ) : (
+        <p>Ничего нет</p>
+      )}
     </>
   )
 }
