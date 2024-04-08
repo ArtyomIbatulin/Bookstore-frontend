8fb951105f91b6ce0a0a4ce9b980e0934dc6fdec
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-08 19:11:57 +0300

    added typography comp

---
+++ src/components/typography/index.tsx
@@ -0,0 +1,8 @@
+type Props = {
+  children: string
+  size?: string
+}
+
+export const Typography: React.FC<Props> = ({ children, size = "text-xl" }) => {
+  return <p className={`${size}`}>{children}</p>
+}
