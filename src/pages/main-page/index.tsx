d15d364c377ab9134adb661da499e8f8dc718b41
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-08 20:34:26 +0300

    fix added cards with books to main page

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,4 +1,5 @@
 import { useFindBooksQuery } from "../../app/services/booksApi"
+import { Card } from "../../components/card"
 import { CreateBook } from "../../components/create-book"
 
 export const MainPage = () => {
@@ -9,6 +10,19 @@ export const MainPage = () => {
       <div className="mb-10 w-full">
         <CreateBook />
       </div>
+      {data && data.length > 0
+        ? data.map(({ id, name, img, price, description }) => (
+            <Card
+              key={id}
+              id={id}
+              cardFor="book"
+              name={name ?? ""}
+              img={img ?? ""}
+              price={price}
+              description={description ?? ""}
+            />
+          ))
+        : null}
     </>
   )
 }
