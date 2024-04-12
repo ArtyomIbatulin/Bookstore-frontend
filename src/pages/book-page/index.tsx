c2a1ace4ddc30bb22db7ba58edf83607296fc0bd
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-12 19:49:37 +0300

    added card for bookPage, added ArrowBack to bookPage

--- src/pages/book-page/index.tsx
+++ src/pages/book-page/index.tsx
@@ -1,3 +1,29 @@
+import { useParams } from "react-router-dom"
+import { useFindBookQuery } from "../../app/services/booksApi"
+import { Card } from "../../components/card"
+import { ArrowBack } from "../../components/arrow-back"
+
 export const BookPage = () => {
-  return <div>BookPage</div>
+  const params = useParams<{ id: string }>()
+  const { data } = useFindBookQuery(params?.id ?? "")
+
+  if (!data) {
+    return <h2>Книги нет</h2>
+  }
+
+  const { id, name, description, img, price } = data
+
+  return (
+    <>
+      <ArrowBack />
+      <Card
+        cardFor="current-book"
+        id={id}
+        name={name ?? ""}
+        description={description ?? ""}
+        img={img ?? ""}
+        price={price}
+      />
+    </>
+  )
 }
