54e1c46716d450ee5ae5441d5a3cb6f90fadba87
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-12 19:48:39 +0300

    added comp ArrowBack

---
+++ src/components/arrow-back/index.tsx
@@ -0,0 +1,19 @@
+import { FaRegArrowAltCircleLeft } from "react-icons/fa"
+import { useNavigate } from "react-router-dom"
+
+export const ArrowBack = () => {
+  const navigate = useNavigate()
+  const handleGoBack = () => {
+    navigate(-1)
+  }
+
+  return (
+    <div
+      onClick={handleGoBack}
+      className="text-default-500 flex items-center gap-2 mb-10 cursor-pointer"
+    >
+      <FaRegArrowAltCircleLeft />
+      Назад
+    </div>
+  )
+}
