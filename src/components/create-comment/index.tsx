b8dbc8470375f1d3be6c23752c14a657965c5f2a
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-04-17 20:39:28 +0300

    added createComment comp

---
+++ src/components/create-comment/index.tsx
@@ -0,0 +1,70 @@
+import { Controller, useForm } from "react-hook-form"
+import { useLazyFindBookQuery } from "../../app/services/booksApi"
+import { Button, Textarea } from "@nextui-org/react"
+import { ErrorMessage } from "../error-message"
+import { IoMdCreate } from "react-icons/io"
+import { useState } from "react"
+import { hasErrorField } from "../../utils/has-error-field"
+import { useCreateCommentMutation } from "../../app/services/commentsApi"
+import { useParams } from "react-router-dom"
+
+export const CreateComment = () => {
+  const { id } = useParams<{ id: string }>()
+  const [createComment] = useCreateCommentMutation()
+  const [getBookById] = useLazyFindBookQuery()
+  const [error, setError] = useState("")
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+  } = useForm()
+
+  // const error = errors?.book?.message as string
+
+  const onSubmit = handleSubmit(async data => {
+    try {
+      if (id) {
+        await createComment({ text: data.comment, bookId: id }).unwrap()
+        setValue("comment", "")
+        await getBookById(id).unwrap()
+      }
+    } catch (error) {
+      if (hasErrorField(error)) {
+        setError(error.data.error)
+      }
+    }
+  })
+
+  return (
+    <form className="flex-grow" onSubmit={onSubmit}>
+      <Controller
+        name="comment"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: "Обязательное поле",
+        }}
+        render={({ field }) => (
+          <Textarea
+            {...field}
+            labelPlacement="outside"
+            placeholder="Напишите комментарий"
+            className="mb-5"
+          />
+        )}
+      />
+      {errors && <ErrorMessage error={error} />}{" "}
+      {/* Так же и в других формах с errors */}
+      <Button
+        color="primary"
+        className="flex-end"
+        endContent={<IoMdCreate />}
+        type="submit"
+      >
+        Комментарий
+      </Button>
+    </form>
+  )
+}
