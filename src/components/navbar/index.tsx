7d1c3951fb4a06a0aa2cf1ddc48bfdba2a5a223e
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2025-07-28 23:23:46 +0300

    feat: edit Navbar comp

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,6 +2,7 @@ import { BsPostcard } from "react-icons/bs"
 import { NavButton } from "../nav-button"
 import { FiUsers } from "react-icons/fi"
 import { FaUsers } from "react-icons/fa"
+import { Fa6 } from "react-icons/fa6"
 
 export const Navbar = () => {
   return (
@@ -16,6 +17,9 @@ export const Navbar = () => {
           </NavButton>
           <NavButton href="/wishlist" icon={<FaUsers />}>
             Лайки
+          </NavButton>
+          <NavButton href="/profile" icon={<Fa6 />}>
+            Профиль
           </NavButton>
         </li>
       </ul>
