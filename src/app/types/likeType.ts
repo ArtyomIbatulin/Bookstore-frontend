13c233489beaf5ca42307182831d5f983671aeeb
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-02-06 19:11:26 +0300

    fix: fix like type

--- src/app/types/likeType.ts
+++ src/app/types/likeType.ts
@@ -1,6 +1,10 @@
+import { Post } from "./postType"
+import { User } from "./userType"
+
 export type Like = {
   id: string
+  user: User
   userId: string
-  bookId: string
-  // user book
+  post: Post
+  postId: string
 }
