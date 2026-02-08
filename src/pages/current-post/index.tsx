457724bc290817436b35677f7e7efdf530f84ffe
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-02-08 18:59:13 +0300

    feat: add current-page

--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,5 +1,41 @@
 import React from "react"
+import { useParams } from "react-router-dom"
+import { useGetPostByIdQuery } from "../../app/services/postApi"
+import { Card } from "../../components/card"
 
 export const CurrentPost = () => {
-  return <div>CurrentPost</div>
+  const params = useParams<{ id: string }>()
+  const { data } = useGetPostByIdQuery(params?.id ?? "")
+
+  if (!data) {
+    return <h2>Поста не существует!</h2>
+  }
+
+  const {
+    id,
+    author,
+    authorId,
+    comments,
+    content,
+    likedByUser,
+    likes,
+    createdAt,
+  } = data
+
+  return (
+    <>
+      <Card
+        id={id}
+        cardFor="current-post"
+        name={author.name ?? ""}
+        avatarUrl={author.avatarUrl ?? ""}
+        authorId={authorId}
+        content={content}
+        commentsCount={comments.length}
+        likesCount={likes.length}
+        likedByUser={likedByUser}
+        createdAt={createdAt}
+      />
+    </>
+  )
 }
