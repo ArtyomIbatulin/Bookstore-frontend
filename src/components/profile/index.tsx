2b0d65d5af08d49a3e1c06b7db1a23fdb780b079
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-02-08 18:44:16 +0300

    feat(profile): add avatar, name and email to profile card

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,8 +1,9 @@
-import { Card, CardBody, CardHeader } from "@nextui-org/react"
+import { Card, CardBody, CardHeader, Image } from "@nextui-org/react"
 import { useAppSelector } from "../../app/hooks"
 import { selectCurrent } from "../../features/user/userSlice"
 import { Link } from "react-router-dom"
 import { MdAlternateEmail } from "react-icons/md"
+import { BASE_URL } from "../../constants"
 
 export const Profile = () => {
   const current = useAppSelector(selectCurrent)
@@ -11,20 +12,25 @@ export const Profile = () => {
     return null
   }
 
-  const { id, login } = current // name, avatar
+  const { id, email, name, avatarUrl } = current
 
   return (
     <Card className="py-4 w-[302px]">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
-        {/* Image for avatar */}
+        <Image
+          alt="Card profile"
+          className="object-cover rounded-xl"
+          src={`${BASE_URL}${avatarUrl}`}
+          width={370}
+        />
       </CardHeader>
       <CardBody>
         <Link to={`/users/${id}`}>
-          <h4 className="font-bold text-large mb-2">{login}</h4> {/* name */}
+          <h4 className="font-bold text-large mb-2">{name}</h4>
         </Link>
         <p className="text-default-500 flex items-center gap-2">
           <MdAlternateEmail />
-          {login}
+          {email}
         </p>
       </CardBody>
     </Card>
