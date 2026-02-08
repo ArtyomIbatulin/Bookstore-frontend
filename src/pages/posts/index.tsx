7444f4774874e7af16e94d1fd8cd1e4e328776c6
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-02-08 19:01:16 +0300

    feat: add posts page content

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,45 @@
 import React from "react"
+import { useGetAllPostsQuery } from "../../app/services/postApi"
+import { CreatePost } from "../../components/create-post"
+import { Card } from "../../components/card"
 
 export const Posts = () => {
-  return <div>Posts</div>
+  const { data } = useGetAllPostsQuery()
+
+  return (
+    <>
+      <div className="mb-10 w-full">
+        <CreatePost />
+      </div>
+
+      {data && data.length > 0
+        ? data.map(
+            ({
+              id,
+              author,
+              authorId,
+              comments,
+              content,
+              likes,
+              likedByUser,
+              createdAt,
+            }) => (
+              <Card
+                key={id}
+                id={id}
+                cardFor="post"
+                name={author.name ?? ""}
+                authorId={authorId}
+                avatarUrl={author.avatarUrl ?? ""}
+                commentsCount={comments.length}
+                likesCount={likes.length}
+                content={content}
+                likedByUser={likedByUser}
+                createdAt={createdAt}
+              />
+            ),
+          )
+        : null}
+    </>
+  )
 }
