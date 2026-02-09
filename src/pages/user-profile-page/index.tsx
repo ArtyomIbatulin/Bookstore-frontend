2ddfd1aa199bfdcf2c9e3c587b17adbdb56bc9d7
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-02-09 21:39:53 +0300

    feat(UserProfilePage): add buttons follow, unfollow, info buttons

--- src/pages/user-profile-page/index.tsx
+++ src/pages/user-profile-page/index.tsx
@@ -13,6 +13,16 @@ import { BASE_URL } from "../../constants"
 import { CiEdit } from "react-icons/ci"
 import { ProfileInfo } from "../../components/profile-info"
 import { EditProfile } from "../../components/edit-profile"
+import {
+  useFollowUserMutation,
+  useUnFollowUserMutation,
+} from "../../app/services/followApi"
+import {
+  MdOutlinePersonAddAlt1,
+  MdOutlinePersonAddDisabled,
+} from "react-icons/md"
+import { formatDate } from "../../utils/format-date"
+import { CountInfo } from "../../components/count-info"
 
 export const UserProfilePage = () => {
   const { id } = useParams<{ id: string }>()
@@ -21,6 +31,8 @@ export const UserProfilePage = () => {
   const { data } = useGetUserByIdQuery(id ?? "")
   const [triggerGetUserByIdQuery] = useLazyGetUserByIdQuery()
   const [triggerCurrentQuery] = useLazyCurrentQuery()
+  const [followUser] = useFollowUserMutation()
+  const [unfollowUser] = useUnFollowUserMutation()
 
   const dispatch = useAppDispatch()
 
@@ -54,30 +66,49 @@ export const UserProfilePage = () => {
         <Card className="flex flex-col items-center text-center space-y-4 p-5 flex-2">
           <Image
             src={`${BASE_URL}${data.avatarUrl}`}
-            alt={data.login} /* name */
+            alt={data.name}
             width={200}
             height={200}
             className="border-4 border-white"
           />
           <div className="flex flex-col text-2xl font-bold gap-4 items-center">
-            {data.login} {/* name */}
-            {currentUser?.id === id && (
-              <Button endContent={<CiEdit />} onClick={() => onOpen()}>
-                Редактировать
+            {data.name}
+            {currentUser?.id === id ? (
+              <Button
+                color={data.isFollowing ? "default" : "primary"}
+                variant="flat"
+                className="gap-2"
+                endContent={
+                  data.isFollowing ? (
+                    <MdOutlinePersonAddDisabled />
+                  ) : (
+                    <MdOutlinePersonAddAlt1 />
+                  )
+                }
+              >
+                {data.isFollowing ? "Отписаться" : "Подписаться"}
               </Button>
+            ) : (
+              <Button endContent={<CiEdit />}>Редактировать</Button>
             )}
           </div>
         </Card>
         <Card className="flex flex-col space-y-4 p-5 flex-1">
-          <ProfileInfo title="Почта" info={data.login} />
-          {/*Плюс другие данные о профиле пользователя*/}
+          <ProfileInfo title="Почта" info={data.email} />
+          <ProfileInfo title="Местоположение" info={data.location} />
+          <ProfileInfo
+            title="Дата рождения"
+            info={formatDate(data.dateOfBirth)}
+          />
+          <ProfileInfo title="Обо мне" info={data.bio} />
+
+          <div className="flex gap-2">
+            <CountInfo count={data.followers.length} title="Подписчики" />
+            <CountInfo count={data.following.length} title="Подписки" />
+          </div>
         </Card>
       </div>
       <EditProfile isOpen={isOpen} onClose={handleClose} user={data} />
     </>
   )
 }
-
-// === == currentUser?.id === id
-// обновить профиль на каждое обновление инпута в форме
-// чек все файлы
