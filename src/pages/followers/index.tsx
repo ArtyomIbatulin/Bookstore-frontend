f2b7615fbbb977646bb5b77d20bb22215ce5bbdd
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-02-15 23:52:19 +0300

    feat: Followers page

--- src/pages/followers/index.tsx
+++ src/pages/followers/index.tsx
@@ -1,5 +1,33 @@
 import React from "react"
+import { useAppSelector } from "../../app/hooks"
+import { selectCurrent } from "../../features/user/userSlice"
+import { Link } from "react-router-dom"
+import { Card, CardBody } from "@nextui-org/react"
+import { User } from "../../components/user"
 
 export const Followers = () => {
-  return <div>Followers</div>
+  const currentUser = useAppSelector(selectCurrent)
+  if (!currentUser) {
+    return null
+  }
+
+  return currentUser.followers.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.followers.map(user => (
+        <Link to={`/users/${user.follower.id}`} key={user.follower.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.follower.name ?? ""}
+                avatarUrl={user.follower.avatarUrl ?? ""}
+                description={user.follower.email ?? ""}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h1>У вас нет подписчиков</h1>
+  )
 }
