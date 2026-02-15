3c5e2a0da14d57ffe22b1d842b66c5768eb7ce85
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2026-02-15 23:57:21 +0300

    feat: add Following page

--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -1,5 +1,33 @@
 import React from "react"
+import { useAppSelector } from "../../app/hooks"
+import { selectCurrent } from "../../features/user/userSlice"
+import { Link } from "react-router-dom"
+import { Card, CardBody } from "@nextui-org/react"
+import { User } from "../../components/user"
 
 export const Following = () => {
-  return <div>Following</div>
+  const currentUser = useAppSelector(selectCurrent)
+  if (!currentUser) {
+    return null
+  }
+
+  return currentUser.following.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.following.map(user => (
+        <Link to={`/users/${user.following.id}`} key={user.following.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.following.name ?? ""}
+                avatarUrl={user.following.avatarUrl ?? ""}
+                description={user.following.email ?? ""}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h1>У вас нет подписок</h1>
+  )
 }
